6018fb00df9a945745a3b5d6433bf32efaf6c0f2
SRIYADHAV R <sriyadhav08@gmail.com>
2026-07-09 10:04:04 +0530

    Create 824-NumberOfLinesToWriteString

---
+++ 824-NumberOfLinesToWriteString/824-NumberOfLinesToWriteString.java
@@ -0,0 +1,24 @@
+// Last updated: 7/9/2026, 10:04:01 AM
+class Solution {
+    public int[] numberOfLines(int[] widths, String s) {
+        if(s.length()==0)
+        return new int[]{0,0};
+        int[] ans=new int[]{1,0};
+        int sum=0;
+        for(int i=0;i<s.length();i++)
+        {
+            int temp=widths[s.charAt(i)-'a'];
+            if(temp+sum<=100)
+            {
+                sum+=temp;
+            }
+            else 
+            {
+                ans[0]++;
+                sum=temp;
+            }
+        }
+        ans[1]=sum;
+        return ans;
+    }
+}
