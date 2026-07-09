9dbd90f81787ac81ee02350aa05dd089b4a0e7a9
SRIYADHAV R <sriyadhav08@gmail.com>
2026-07-09 10:02:41 +0530

    Create 1848-SumOfUniqueElements

---
+++ 1848-SumOfUniqueElements/1848-SumOfUniqueElements.java
@@ -0,0 +1,18 @@
+// Last updated: 7/9/2026, 10:02:38 AM
+class Solution {
+    public int sumOfUnique(int[] nums) {
+      int sum=0;
+      for(int i=0;i<nums.length;i++){
+        int count=0;
+        for(int j=0;j<nums.length;j++){
+            if(nums[i]==nums[j]){
+                count++;
+            }
+        }
+        if(count==1){
+            sum+=nums[i];
+        }
+      } 
+      return sum; 
+    }
+}
