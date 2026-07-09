bb9a610c18104d269600a1da7cc446ee827fae0d
SRIYADHAV R <sriyadhav08@gmail.com>
2026-07-09 10:03:55 +0530

    Create 862-FindAndReplaceInString

---
+++ 862-FindAndReplaceInString/862-FindAndReplaceInString.java
@@ -0,0 +1,22 @@
+// Last updated: 7/9/2026, 10:03:52 AM
+class Solution {
+    public String findReplaceString(String s, int[] indices, String[] sources, String[] targets) {
+        Map<Integer,Integer> map=new HashMap<>();
+        for (int i=0; i<indices.length; i++){
+            if (s.startsWith(sources[i],indices[i])){
+                map.put(indices[i],i);
+            }
+        }  
+        StringBuilder sb=new StringBuilder();
+        for (int i=0; i<s.length();){
+            if (!map.containsKey(i)){
+                sb.append(s.charAt(i));
+                i++;
+            } else { //replace chars
+                sb.append(targets[map.get(i)]); 
+                i+=sources[map.get(i)].length();
+            }
+        }
+        return sb.toString();
+    }
+}
