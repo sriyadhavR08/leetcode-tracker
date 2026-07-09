948d716dfc2054d120aafc820ef8f5688a4101f6
SRIYADHAV R <sriyadhav08@gmail.com>
2026-07-09 10:01:32 +0530

    Create 4229-TrimTrailingVowels

---
+++ 4229-TrimTrailingVowels/4229-TrimTrailingVowels.java
@@ -0,0 +1,16 @@
+// Last updated: 7/9/2026, 10:01:29 AM
+class Solution {
+    public String trimTrailingVowels(String s) {
+        int i = s.length() - 1;
+
+        while (i >= 0 && (s.charAt(i) == 'a' || 
+                          s.charAt(i) == 'e' || 
+                          s.charAt(i) == 'i' || 
+                          s.charAt(i) == 'o' || 
+                          s.charAt(i) == 'u')) {
+            i--;
+        }
+
+        return s.substring(0, i + 1);
+    }
+}
