6849c4ccb55ffa5a0aebad7dff3fabbbcacf6810
SRIYADHAV R <sriyadhav08@gmail.com>
2026-07-09 10:01:46 +0530

    Create 4052-EqualScoreSubstrings

---
+++ 4052-EqualScoreSubstrings/4052-EqualScoreSubstrings.java
@@ -0,0 +1,13 @@
+// Last updated: 7/9/2026, 10:01:43 AM
+class Solution {
+    public boolean scoreBalance(String s) {
+        int total = 0;
+        for (char c : s.toCharArray()) total += c - 'a' + 1;
+        int prefix = 0;
+        for (char c : s.toCharArray()) {
+            prefix += c - 'a' + 1;
+            if (2 * prefix == total) return true;
+        }
+        return false;
+    }
+}
