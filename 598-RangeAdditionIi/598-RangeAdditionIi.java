0d577d6a9eed46b322716522cada1203490113b1
SRIYADHAV R <sriyadhav08@gmail.com>
2026-07-09 10:04:56 +0530

    Create 598-RangeAdditionIi

---
+++ 598-RangeAdditionIi/598-RangeAdditionIi.java
@@ -0,0 +1,13 @@
+// Last updated: 7/9/2026, 10:04:53 AM
+class Solution {
+    public int maxCount(int m, int n, int[][] ops) {
+         if (ops.length == 0) return m * n;
+        int minRow = m;
+        int minCol = n;
+        for (int[] op : ops) {
+            minRow = Math.min(minRow, op[0]);
+            minCol = Math.min(minCol, op[1]);
+        }
+        return minRow * minCol;
+    }
+}
