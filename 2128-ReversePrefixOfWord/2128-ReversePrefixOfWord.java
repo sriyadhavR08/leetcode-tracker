a29a8f1b53bd838f974914662f8d83fa22a762cf
SRIYADHAV R <sriyadhav08@gmail.com>
2026-07-09 10:02:23 +0530

    Create 2128-ReversePrefixOfWord

---
+++ 2128-ReversePrefixOfWord/2128-ReversePrefixOfWord.java
@@ -0,0 +1,10 @@
+// Last updated: 7/9/2026, 10:02:20 AM
+class Solution {
+    public String reversePrefix(String word, char ch) {
+        int j = word.indexOf(ch);
+        if (j != -1) {
+            return new StringBuilder(word.substring(0, j + 1)).reverse().toString() + word.substring(j + 1);
+        }
+        return word;
+    }
+}
