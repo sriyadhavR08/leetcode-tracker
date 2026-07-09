6c67414ef7ccdf1f7a35b3fc9ac2d7f9a8d7e9c8
SRIYADHAV R <sriyadhav08@gmail.com>
2026-07-09 10:03:33 +0530

    Create 1055-PairsOfSongsWithTotalDurationsDivisibleBy60

---
+++ 1055-PairsOfSongsWithTotalDurationsDivisibleBy60/1055-PairsOfSongsWithTotalDurationsDivisibleBy60.java
@@ -0,0 +1,18 @@
+// Last updated: 7/9/2026, 10:03:30 AM
+class Solution {
+    public int numPairsDivisibleBy60(int[] time) {
+        int n = time.length, ans = 0;
+        HashMap<Integer, Integer> len = new HashMap<>();
+        for(int i  = 0; i < n; i++){
+            int rem = time[i] % 60;
+            int target = 60 - (rem);
+            if(len.containsKey(target))
+                ans += len.get(target);
+            if(rem != 0)
+                len.put(rem, len.getOrDefault(rem, 0) + 1);
+            else
+                len.put(60, len.getOrDefault(60, 0) + 1);
+        }
+        return ans;
+    }
+}
