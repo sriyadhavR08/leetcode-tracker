eb1c824997e08a3b441d490be072a5da30c9b535
SRIYADHAV R <sriyadhav08@gmail.com>
2026-07-09 10:03:35 +0530

    Create 1049-MinimumDominoRotationsForEqualRow

---
+++ 1049-MinimumDominoRotationsForEqualRow/1049-MinimumDominoRotationsForEqualRow.java
@@ -0,0 +1,21 @@
+// Last updated: 7/9/2026, 10:03:32 AM
+class Solution {
+    public int minDominoRotations(int[] tops, int[] bottoms) {
+        int res = Integer.MAX_VALUE;
+        for (int val = 1; val <= 6; val++) {
+            int topSwaps = 0, bottomSwaps = 0;
+            boolean valid = true;
+            for (int i = 0; i < tops.length; i++) {
+                if (tops[i] != val && bottoms[i] != val) {
+                    valid = false;
+                    break;
+                }
+                if (tops[i] != val) topSwaps++;
+                if (bottoms[i] != val) bottomSwaps++;
+            }
+            if (valid)
+                res = Math.min(res, Math.min(topSwaps, bottomSwaps));
+        }
+        return res == Integer.MAX_VALUE ? -1 : res;
+    }
+}
