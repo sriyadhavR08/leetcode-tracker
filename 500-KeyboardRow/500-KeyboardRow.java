366ef25b30a6305471e6b571831998e2f2000fee
SRIYADHAV R <sriyadhav08@gmail.com>
2026-07-09 10:05:24 +0530

    Create 500-KeyboardRow

---
+++ 500-KeyboardRow/500-KeyboardRow.java
@@ -0,0 +1,22 @@
+// Last updated: 7/9/2026, 10:05:21 AM
+class Solution {
+    public String[] findWords(String[] words) {
+         ArrayList<String> ans=new ArrayList<>();
+       String first = "qwertyuiop";
+       String Secound ="asdfghjkl";
+       String Third="zxcvbnm";
+       for(String i : words){
+        if(isinrow(i,first) || isinrow(i,Secound) || isinrow(i,Third))
+         ans.add(i);
+       } 
+       return ans.toArray(new String[0]);
+    }
+    private boolean isinrow(String s,String row){
+      for(char c:s.toCharArray()){
+        if(row.indexOf(Character.toLowerCase(c))==-1){
+            return false;
+        }
+      }
+      return true;
+    }
+}
