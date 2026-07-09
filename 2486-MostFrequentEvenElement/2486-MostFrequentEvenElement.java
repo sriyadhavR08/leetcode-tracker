bb0f136dabb9b5c844f28bfd7102cb78e4516318
SRIYADHAV R <sriyadhav08@gmail.com>
2026-07-09 10:02:09 +0530

    Create 2486-MostFrequentEvenElement

---
+++ 2486-MostFrequentEvenElement/2486-MostFrequentEvenElement.java
@@ -0,0 +1,21 @@
+// Last updated: 7/9/2026, 10:02:06 AM
+class Solution {
+    public int mostFrequentEven(int[] nums) {
+        int n = nums.length;
+        int freq[] = new int[100001];
+        int ans = -1, maxFreq = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] % 2 == 0) {
+                freq[nums[i]]++;
+                if (freq[nums[i]] == maxFreq) {
+                    ans = Math.min(ans, nums[i]);
+                } else if (freq[nums[i]] > maxFreq) {
+                    ans = nums[i];
+                    maxFreq = freq[nums[i]];
+                }
+            }
+        }
+
+        return ans;
+    }
+}
