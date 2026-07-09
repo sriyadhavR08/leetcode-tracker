51edc2e67c1810ca94504869ee6401f7dcba48de
SRIYADHAV R <sriyadhav08@gmail.com>
2026-07-09 10:02:12 +0530

    Create 2337-RemoveDigitFromNumberToMaximizeResult

---
+++ 2337-RemoveDigitFromNumberToMaximizeResult/2337-RemoveDigitFromNumberToMaximizeResult.java
@@ -0,0 +1,14 @@
+// Last updated: 7/9/2026, 10:02:09 AM
+class Solution {
+    public String removeDigit(String number, char digit) {   
+        List<String> digits = new ArrayList<>();
+        for (int i = 0; i < number.length(); i++) {
+            if (number.charAt(i) == digit) {
+                String stringWithoutDigit = number.substring(0, i) + number.substring(i + 1);
+                digits.add(stringWithoutDigit);
+            }
+        }        
+        Collections.sort(digits);
+        return digits.get(digits.size() - 1);
+    }
+}
