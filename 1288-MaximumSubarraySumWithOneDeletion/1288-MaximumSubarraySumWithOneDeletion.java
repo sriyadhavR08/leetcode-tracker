48169ed7a1f738d32eb1b976589e79341ec97352
SRIYADHAV R <sriyadhav08@gmail.com>
2026-07-09 10:03:20 +0530

    Create 1288-MaximumSubarraySumWithOneDeletion

---
+++ 1288-MaximumSubarraySumWithOneDeletion/1288-MaximumSubarraySumWithOneDeletion.java
@@ -0,0 +1,24 @@
+// Last updated: 7/9/2026, 10:03:17 AM
+class Solution {
+    public int maximumSum(int[] arr) {
+        int n = arr.length;
+        int[] maxEndHere = new int[n];
+        int[] maxStartHere = new int[n];
+
+        int max = arr[0];
+
+        maxEndHere[0] = arr[0];
+
+        for(int i = 1;i<n;i++){
+            maxEndHere[i] = Math.max(arr[i], maxEndHere[i-1] + arr[i]);
+            max = Math.max(max, maxEndHere[i]);
+        }
+
+         maxStartHere[n-1] = arr[n-1];
+        for(int i=n-2; i >= 0; i--)
+            maxStartHere[i] = Math.max(arr[i], maxStartHere[i+1]+arr[i]);
+        for(int i=1; i < n-1; i++)
+            max = Math.max(max, maxEndHere[i-1]+maxStartHere[i+1]);
+        return max;
+    }
+}
